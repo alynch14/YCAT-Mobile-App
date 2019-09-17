eef515aaf53eb1c5fe335d6b7d2a0f75076ec99b
Adam Lynch <alynch14@students.towson.edu>
2019-09-17 17:27:41 -0400

    Created full path from Home through Snacks.

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponentInParent<AudioSource>();
+        audioClip.LoadAudioData();
+    }
+
+    public void OnMouseDown()
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
