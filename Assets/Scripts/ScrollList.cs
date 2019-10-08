97ec7be2f516c191510a6be421af69349ca3be90
Adam Lynch <alynch14@students.towson.edu>
2019-10-08 10:50:13 -0400

    Implemented scroll view

---
+++ Assets/Scripts/ScrollList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public Button button;
+}
+
+public class ScrollList : MonoBehaviour
+{
+    public List<Item> buttons;
+    public Transform contentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
